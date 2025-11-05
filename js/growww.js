300ec54da93dd542ad13950bf75d3c3663414987
Abhishek Dutta <tools@growww.in>
2025-11-05 18:26:52 +0530

    Enhance footer spacing adjustments for mobile and larger screens in growww.js

--- js/growww.js
+++ js/growww.js
@@ -5,10 +5,16 @@ $(document).ready(function () {
       $('.tp-offcanvas-area .tp-offcanvas-contact, .tp-offcanvas-area .tp-copyright-2-social, .tp-offcanvas-area .tp-offcanvas-top h1, .tp-offcanvas-area .tp-offcanvas-top img, .tp-offcanvas-area .tp-offcanvas-content .tp-offcanvas-title').addClass('d-none');
       
       $(".tp-offcanvas-open-btn").removeClass("d-none");
+
+      // replace footer spacing for smaller screens
+      $('.tp-footer-2-area .col-xl-3, .tp-footer-2-area .col-xl-4').removeClass('mb-50').addClass('mb-5');
     } else {
       // $('.tp-offcanvas-area .tp-offcanvas-top').removeClass('d-none').addClass('d-flex');
       $('.tp-offcanvas-area .tp-offcanvas-contact, .tp-offcanvas-area .tp-copyright-2-social,.tp-offcanvas-area .tp-offcanvas-top h1,.tp-offcanvas-area .tp-offcanvas-top img, .tp-offcanvas-area .tp-offcanvas-content .tp-offcanvas-title').removeClass('d-none');
       $(".tp-offcanvas-open-btn").addClass("d-none");
+
+      // restore original footer spacing on larger screens
+      $('.tp-footer-2-area .col-xl-3, .tp-footer-2-area .col-xl-4').removeClass('mb-5').addClass('mb-50');
     }
   }
 
